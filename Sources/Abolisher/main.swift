17d37c09cab5bfa739b2314881ccf43e96791943
Jan Dammshäuser <mail@jandamm.de>
2020-03-11 21:31:01 +0100

    Add nicer error handling for user errors

--- Sources/Abolisher/main.swift
+++ Sources/Abolisher/main.swift
@@ -2,11 +2,18 @@ import Foundation
 import Library
 
 func echo(_ str: String) { print(str) }
+func echoErr(_ str: String) { FileHandle.standardError.write(Data(str.utf8)) }
 
-try CommandLine.arguments.dropFirst()
-	.map(URL.init(fileURLWithPath:))
-	.map(String.init(contentsOf:))
-	.map { $0.components(separatedBy: .newlines) }
-	.flatMap(parse)
-	.flatMap(expand)
-	.forEach(echo)
+do {
+	try CommandLine.arguments.dropFirst()
+		.map(URL.init(fileURLWithPath:))
+		.map(String.init(contentsOf:))
+		.map { $0.components(separatedBy: .newlines) }
+		.flatMap(parse)
+		.flatMap(expand)
+		.forEach(echo)
+} catch let Abolisher.Error.replaceMissing(line) {
+	echoErr("There is no Replace detected in this line:\n\(line)")
+} catch {
+	throw error
+}
